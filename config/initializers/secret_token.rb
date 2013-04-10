406ee5b9379a3546626a4556a2b296a5fd251d48
Simonini <descovi@gmail.com>
2013-04-10 19:45:25 +0200

    start with quella volta che

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuellaVoltaChe::Application.config.secret_token = '8720e38c8d156bbc848f5871a4beed4b5c712ee67e72f904588e3c99ffe594153a06d26a35479c44194031857a20254117837e9ee78052abb38797b2865a9fe6'
